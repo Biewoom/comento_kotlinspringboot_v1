3ca28918ee280c5a5cf03405ce702338aad68528
Jeongmin Yoo <jungmin.yu@navercorp.com>
2022-08-16 02:39:41 +0900

    setting: add Multimodule project

---
+++ jpa/build.gradle.kts
@@ -0,0 +1,20 @@
+
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("mysql:mysql-connector-java")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "root"
+include("dbless")
+include("jpa")
