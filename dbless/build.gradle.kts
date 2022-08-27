96e809ff6f6560b475699eeead829b152fec7c54
DESKTOP-UCAG60S\Administrator <ok4343@hyundai-ite.com>
2022-08-27 21:52:50 +0900

    멀티 모듈 프로젝트 구성 완료

---
+++ dbless/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
