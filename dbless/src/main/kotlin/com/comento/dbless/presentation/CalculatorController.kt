030d8d3b8ba2e9127e9ef0d9f8a84679ea2e7f9d
luvimperfection <luvimperfection@gmail.com>
2022-08-28 20:13:10 +0900

    Calculator WIP

--- dbless/src/main/kotlin/com/comento/dbless/presentation/CalculatorController.kt
+++ dbless/src/main/kotlin/com/comento/dbless/presentation/CalculatorController.kt
@@ -1,8 +1,11 @@
 package com.comento.dbless.presentation
 
+import com.comento.dbless.presentation.dto.CalculatorRequest
 import com.comento.dbless.service.CalculatorService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,6 +18,12 @@ class CalculatorController(
     fun generateRandomNumber(@PathVariable("range") range: String): Number {
         val (start, end) = range.filter { !it.isWhitespace() }.split("~")
         return calculatorService.generateRandomNumber(start, end)
+    }
+
+    @PostMapping("/calculate")
+    fun calculateNumber(@RequestBody request: CalculatorRequest): Double {
+        val (expression, round) = request
+        return calculatorService.calculate(expression, round)
     }
 
 }
--- dbless/src/main/kotlin/com/comento/dbless/service/CalculatorService.kt
+++ dbless/src/main/kotlin/com/comento/dbless/service/CalculatorService.kt
@@ -3,12 +3,15 @@ package com.comento.dbless.service
 import com.comento.dbless.getDecimalPoint
 import com.comento.dbless.toNumber
 import com.comento.dbless.toRound
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.lang.Integer.max
+import kotlin.math.pow
 import kotlin.random.Random
 
 @Service
 class CalculatorService {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     fun generateRandomNumber(start: String, end: String): Number {
         val startNum = start.toNumber()
@@ -23,4 +26,56 @@ class CalculatorService {
                 .toRound(max(startNumDecimalPoint, endNumDecimalPoint))
         }
     }
+
+    fun calculate(expression: String, round: Int): Double {
+        return parseExpression(expression).toRound(round)
+    }
+
+    private fun parseExpression(expression: String): Double {
+        val filteredExpression = expression.filter { !it.isWhitespace() }.toList()
+        val calculatingBlocks = mutableListOf<String>()
+        var index = 0
+        while(index < filteredExpression.size){
+            if(filteredExpression.get(index) in listOf('+', '-', '*', '/', '^')){
+                calculatingBlocks.add(filteredExpression[index].toString())
+                index += 1
+            }else{
+                val numberBlock = filteredExpression.subList(fromIndex = index, toIndex = filteredExpression.size).takeWhile { it.isDigit() }.joinToString("")
+                calculatingBlocks.add(numberBlock)
+                index += numberBlock.length
+            }
+        }
+        logger.info("calculatingBlocks: $calculatingBlocks")
+        return calculateExpression(calculatingBlocks.toList())
+    }
+
+    private fun calculateExpression(expressionChars: List<String>):Double{
+        expressionChars.firstOrNull { it in listOf("+", "-") }?.let {
+            val index = expressionChars.indexOf(it)
+            if(it=="+"){
+                return calculateExpression(expressionChars.subList(0, index)) + calculateExpression(expressionChars.subList(index+1, expressionChars.size))
+            }else{
+                return calculateExpression(expressionChars.subList(0, index)) - calculateExpression(expressionChars.subList(index+1, expressionChars.size))
+            }
+        }
+
+        expressionChars.firstOrNull { it in listOf("*", "/", "^") }?.let {
+            val index = expressionChars.indexOf(it)
+            if(it=="*"){
+                return calculateExpression(expressionChars.subList(0, index)) * calculateExpression(expressionChars.subList(index+1, expressionChars.size))
+            }else if(it=="/"){
+                return calculateExpression(expressionChars.subList(0, index)) / calculateExpression(expressionChars.subList(index+1, expressionChars.size))
+            }else {
+                return calculateExpression(expressionChars.subList(0, index)).pow(calculateExpression(expressionChars.subList(index+1, expressionChars.size)))
+            }
+        }
+
+        if(expressionChars.size==1){
+            return expressionChars.first().toDouble()
+        }
+
+        throw RuntimeException()
+
+    }
+
 }
