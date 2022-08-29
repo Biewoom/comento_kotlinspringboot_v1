e29ce61b503dec1309b5d1976d89eabd5a5ade18
Jeongmin Yoo <jungmin.yu@navercorp.com>
2022-08-30 07:22:02 +0900

    feat: week4 설정 추가

---
+++ jpa/src/main/resources/sql/dml/countries_insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO COUNTRY
+    (ZIP_CODE, NAME, CAPITAL_CITY)
+VALUES
+    (43595, 'USA', 'Washington, D.C.'),
+    (8443,'KOREA', 'Seoul'),
+    (1700000,'CHINA', 'Beijing'),
+    (94378,'JAPAN', 'Tokyo'),
+    (43538,'RUSSIA', 'Moscow'),
+    (59068,'TAIWAN', 'Taipei'),
+    (6553,'VIETNAM', 'Hanoi'),
+    (45366,'IRELAND', 'Dublin'),
+    (1231327,'ENGLAND', 'London'),
+    (5435322,'CANADA', 'Ottawa');
