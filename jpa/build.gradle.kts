2078199f5a4ff91c006058bb121662d5248d9637
Jeongmin Yoo <jungmin.yu@navercorp.com>
2022-08-30 21:14:32 +0900

    chore: OpenAPI 와 KLogger 추가

--- jpa/build.gradle.kts
+++ jpa/build.gradle.kts
@@ -17,4 +17,21 @@ dependencies {
     runtimeOnly("mysql:mysql-connector-java")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // open API
+    implementation("org.springdoc:springdoc-openapi-data-rest:1.5.12")
+    implementation("org.springdoc:springdoc-openapi-ui:1.5.12")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.5.12")
+
+    // KLogger
+    implementation("io.github.microutils:kotlin-logging:2.1.21")
+
+    // log4j2
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+
+    modules {
+        module("org.springframework.boot:spring-boot-starter-logging") {
+            replacedBy("org.springframework.boot:spring-boot-starter-log4j2", "Use Log4j2 instead of Logback")
+        }
+    }
 }
--- jpa/src/main/kotlin/com/comento/jpa/JpaApplication.kt
+++ jpa/src/main/kotlin/com/comento/jpa/JpaApplication.kt
@@ -1,11 +1,13 @@
 package com.comento.jpa
 
+import mu.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
+val logger = KotlinLogging.logger { }
 @SpringBootApplication
 class JpaApplication
-
 fun main(args: Array<String>) {
 	runApplication<JpaApplication>(*args)
 }
