3e00ce120e7c8b9a7257f0ec195554ada6489a25
Jeongmin Yoo <jungmin.yu@navercorp.com>
2022-08-31 03:22:34 +0900

    feat: Gender Enum 클래스 추가

---
+++ jpa/src/main/kotlin/com/comento/jpa/domain/common/enums/Gender.kt
@@ -0,0 +1,15 @@
+package com.comento.jpa.domain.common.enums
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+
+enum class Gender(@JsonValue val genderStr: String) {
+    UNKNOWN("UnKnown"),
+    MALE("Male"),
+    FEMALE("Female");
+
+    companion object {
+        @JvmStatic @JsonCreator
+        operator fun invoke(genderStr: String) = values().find { it.genderStr == genderStr } ?: UNKNOWN
+    }
+}
