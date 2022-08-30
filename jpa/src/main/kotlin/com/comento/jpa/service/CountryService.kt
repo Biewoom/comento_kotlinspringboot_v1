67111267a8de7e63f76ab359b2f5107ca4dd8abb
Jeongmin Yoo <jungmin.yu@navercorp.com>
2022-08-31 01:27:00 +0900

    chore: makeCountry 삭제

--- jpa/src/main/kotlin/com/comento/jpa/service/CountryService.kt
+++ jpa/src/main/kotlin/com/comento/jpa/service/CountryService.kt
@@ -1,28 +1,13 @@
 package com.comento.jpa.service
 
 import com.comento.jpa.domain.CountryNotFoundException
-import com.comento.jpa.domain.country.Country
 import com.comento.jpa.domain.country.CountryRepository
-import com.comento.jpa.domain.person.Gender
-import com.comento.jpa.presentation.CountryRequest
 import org.springframework.stereotype.Service
 
 @Service
 class CountryService(
     private val countryRepository: CountryRepository
 ) {
-
-    fun makeCountry(countryRequest: CountryRequest) {
-        val (zipCode, name, capitalCity) = countryRequest
-
-        val country = Country(
-            zipCode = zipCode,
-            name = name,
-            capitalCity = capitalCity
-        )
-        Gender.FEMALE
-        countryRepository.save(country)
-    }
 
     fun findCapitalCityByCountryName(countryName: String): String {
         val country = countryRepository.findCountryByName(countryName.uppercase()) ?: throw CountryNotFoundException("`$countryName` Cannot be Found")
