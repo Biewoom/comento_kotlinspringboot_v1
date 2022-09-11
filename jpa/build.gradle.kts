7033324641b0c9245e090f0e5576807d39de8adf
luvimperfection <luvimperfection@gmail.com>
2022-09-11 17:23:22 +0900

    TCP H2DB, SQL Init 적용

--- jpa/build.gradle.kts
+++ jpa/build.gradle.kts
@@ -14,8 +14,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    compileOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     // open API
